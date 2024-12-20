3e77e005eaae2cd4458728e34a26f73ed67dc93c
Sebastian Cheung <sebcheung64@gmail.com>
2024-12-19 19:14:01 -0500

    finished rendering the 3d model and in progress of creating cards for the about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
@@ -18,16 +18,33 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Sebastian
               </span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a college student majoring in CS with<br className="sm:block hidden" />
-            a concentration in AI. I hope to build projects<br className="sm:block hidden" />
-            that use AI and Machine Learning techniques while<br className="sm:block hidden" />
-            developing other fun projects as well!<br className="sm:block hidden" />
-            Glad to have you on the journey!
+            Welcome to my Hub!<br className="sm:block hidden" />
+            Here, you can explore my life's work and<br className="sm:block hidden" />
+            passion to the fullest. Feel free to reach out<br className="sm:block hidden" />
+            and enjoy your stay!<br className="sm:block hidden" />
           </p>
         </div>
       </div>
         
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
