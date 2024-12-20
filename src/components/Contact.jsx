8e638f61005667e5b3ecef0a700b52adb7546a5b
Sebastian Cheung <sebcheung64@gmail.com>
2024-12-20 15:39:43 -0500

    finished portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,45 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
   
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_84nr46b', 
+      'template_ues354y',
+      {
+        from_name: form.name,
+        to_name: 'Sebastian',
+        from_email: form.email,
+        to_email: 'sebcheung64@gmail.com',
+        message: form.message,
+      },
+      'CBM-Wuj4o6qF2w0iO'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you ASAP.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex
